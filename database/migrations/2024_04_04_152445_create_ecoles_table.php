0f089b6f8e3aa8d2959ee7eeb5e62c5ac55fe3f6
denisbyte <denisafoh22@gmail.com>
2024-04-04 19:39:25 +0200

    changement au niveau du modèle école

--- database/migrations/2024_04_04_152445_create_ecoles_table.php
+++ database/migrations/2024_04_04_152445_create_ecoles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('ecoles', function (Blueprint $table) {
             $table->id();
-            $table->String(nom);
+            $table->String('nom');
             $table->timestamps();
         });
     }
